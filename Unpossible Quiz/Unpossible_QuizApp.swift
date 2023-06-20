ea571b9b9e2757e241aabcb90c1352f76e74f5e3
Asher McConnell <asherryan7@gmail.com>
2023-06-20 16:13:02 -0600

    Initial Commit

---
+++ Unpossible Quiz/Unpossible_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unpossible_QuizApp.swift
+//  Unpossible Quiz
+//
+//  Created by Asher McConnell on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Unpossible_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
